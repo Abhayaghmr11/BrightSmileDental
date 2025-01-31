543e3258384ec22a1dfe50976daebea61ce33fe0
AnishSherchan <sherchananish11@gmail.com>
2025-01-31 23:02:21 +0545

    First deployment Testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
 import Logo from "@/public/assets/icons/Logo/Logo.svg"
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <>
+    <div className=" max-w-[1440px] mx-auto flex justify-between items-center py-[10px] px-[60px]">
     <Image src={Logo} alt="Logo" />
-    </>
+    <Button variant="default" className=" font-black ml-5">Get Quotation</Button>
+    </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,31 +1,36 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: "#3B82F6",
-        background: '#FFFFFF',
-        secondary: '#F7F7F7',
-        foreground: "#030303",
-        secondary_foreground: "#7B7B7B",
-        border_color: '#D8D8D8'
-      },
-      fontSize: {
-        banner_text: "44px", //?44px
-        heading: "40px",  //?40px
-        sub_heading: '16px', //? 16px
-        paragraph: '14px', //? 14px
-        nav: '15px', //? 15px
-        label: '12px' //? 12 px
-
-      },
-    },
+  	extend: {
+  		colors: {
+  			primary: '#3B82F6',
+  			background: '#FFFFFF',
+  			secondary: '#F7F7F7',
+  			foreground: '#030303',
+  			secondary_foreground: '#7B7B7B',
+  			border_color: '#D8D8D8'
+  		},
+  		fontSize: {
+  			banner_text: '44px',
+  			heading: '40px',
+  			sub_heading: '16px',
+  			paragraph: '14px',
+  			nav: '15px',
+  			label: '12px'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
