6dbd213f349272ee9c5c0d93f3a9fecb123bc907
Abhaya Ghimire <abhaya2047@gmail.com>
2025-01-31 16:42:51 +0000

    Testing

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Logo from "@/public/assets/icons/Logo/Logo.svg"
 export default function Home() {
   return (
     <>
-    <h1 className=" max-w-[588px] md:text-banner_text font-black text-[25px]">Welcome to <span className=" text-primary">Bright Smile</span> Dental Clinic</h1>
     <Image src={Logo} alt="Logo" />
     </>
   );
