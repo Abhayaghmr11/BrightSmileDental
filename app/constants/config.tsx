ba1cbd8e934d17751b09c1b0a10a6ae67174c1ed
Abhayaghmr11 <abhaya2057@gmail.com>
2025-02-04 18:53:49 +0000

    Constants added to config file

---
+++ app/constants/config.tsx
@@ -0,0 +1,203 @@
+
+export const companyEmail = "brightsmiledental@gmail.com";
+export const phoneNumber = "+977 1-4519594";
+
+export const ServiceData = [
+    {
+        category: "Extraction",
+        section: [
+            {
+                service: "Primary Tooth Extraction",
+                details: "",
+                price: "Rs. 500"
+            },
+            {
+                service: "Grade I Extraction",
+                details: "",
+                price: "Rs. 500"
+            },
+            {
+                service: "Grade II Extraction",
+                details: "",
+                price: "Rs. 500"
+            },
+            {
+                service: "Upper Wisdom Tooth (Simple)",
+                details: "",
+                price: "Rs. 500"
+            },
+            {
+                service: "Upper Wisdom Tooth (Complex)",
+                details: "",
+                price: "Rs. 500"
+            },
+            {
+                service: "Surgical Extraction (Including Lower Wisdom Tooth)",
+                details: "",
+                price: "Rs. 500"
+            },
+            {
+                service: "Extraction for Orthodontic Treatment",
+                details: "",
+                price: "Rs. 500"
+            },
+            {
+                service: "Cyst Removal Surgery and APICOECTOMY (Per Tooth by Specialist)",
+                details: "",
+                price: "Rs. 500"
+            },
+        ]
+    },
+    {
+        category:"Endodontics Root Canal Treatment",
+        section:[
+            {
+                service: "Anterior Root Canal Treatment",
+                details: "",
+                price: "Rs. 500"
+            },
+            {
+                service: "Posterior Root Canal Treatment",
+                details: "",
+                price: "Rs. 500"
+            },
+            {
+                service: "Re-Root Canal Treatment",
+                details: "",
+                price: "Rs. 500"
+            },
+            {
+                service: "Single Visit RCT - Anterior",
+                details: "",
+                price: "Rs. 500"
+            },
+            {
+                service: "Single Visit RCT - Posterior",
+                details: "",
+                price: "Rs. 500"
+            },
+            {
+                service: "Instrument Bi-Pass",
+                details: "",
+                price: "Rs. 500"
+            },
+            {
+                service: "Instrument Removal",
+                details: "",
+                price: "Rs. 500"
+            },
+            {
+                service: "Crown Removal",
+                details: "",
+                price: "Rs. 500"
+            },
+            {
+                service: "Pulpectomy",
+                details: "",
+                price: "Rs. 500"
+            },
+            {
+                service: "3RD Molar RCT",
+                details: "",
+                price: "Rs. 500"
+            },
+
+        ]
+    },
+    {
+        category:"Periodontics",
+        section:
+        [
+            {
+                service: "Gingival Abcess Drainage",
+                details: "",
+                price: "Rs. 500"
+            },
+            {
+                service: "Subgingival Scaling I",
+                details: "",
+                price: "Rs. 500"
+            },
+            {
+                service: "Subgingival Scaling II",
+                details: "",
+                price: "Rs. 500"
+            },
+            {
+                service: "Subgingival Scaling III",
+                details: "",
+                price: "Rs. 500"
+            },
+            {
+                service: "Flap Surgery Sextant (Grade I)",
+                details: "",
+                price: "Rs. 500"
+            },
+            {
+                service: "Flap Surgery Sextant (Grade II)",
+                details: "",
+                price: "Rs. 500"
+            },
+            {
+                service: "Flap Surgery Quadrant (Grade I)",
+                details: "",
+                price: "Rs. 500"
+            },
+            {
+                service: "Flap Surgery Quadrant (Grade II)",
+                details: "",
+                price: "Rs. 500"
+            },
+        ]
+    },
+    {
+        category:"Implant",
+        section:[
+            {
+                service: "Implant",
+                details: "",
+                price: "Rs. 500"
+            },
+            {
+                service: "Overlay Retainer",
+                details: "",
+                price: "Rs. 500"
+            },
+            {
+                service: "Implant Maintenance",
+                details: "",
+                price: "Rs. 500"
+            },
+            {
+                service: "SINUS Lift (Including Bone Graft and Membrane)",
+                details: "",
+                price: "Rs. 500"
+            },
+        ]
+    },
+    {
+        category:"Orthodontics",
+        section:[
+            {
+                service: "Orthodontic Treatment (Braces)",
+                details: "",
+                price: "Rs. 500"
+            },
+            {
+                service: "Clear Aligner Upper and Lower",
+                details: "",
+                price: "Rs. 500"
+            },
+            {
+                service: "Overlay Retainer - Per Inch",
+                details: "",
+                price: "Rs. 500"
+            },
+            {
+                service: "Hawleys Appliance - Per Arch",
+                details: "",
+                price: "Rs. 500"
+            },
+        ]
+    }
+]
