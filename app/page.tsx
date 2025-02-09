4b53989b6fd57a763d678f317137f508b76dcb06
AnishSherchan <sherchananish11@gmail.com>
2025-02-10 00:36:28 +0545

    Banner and Contact Form Strated

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import HeroLanding from "@/components/organism/landing/HeroLanding";
 import Services from "@/components/organism/landing/Services";
 import Testimonials from "@/components/organism/landing/Testimonials";
 import Teams from "@/components/organism/landing/Teams";
+import Banner from "@/components/organism/Banner";
+import ContactUs from "@/components/organism/landing/ContactUs";
 
 export default function Home() {
   return (
@@ -9,7 +11,9 @@ export default function Home() {
       <HeroLanding />
       <Testimonials />
       <Services />
-      <Teams/>
+      <Teams />
+      <Banner />
+      <ContactUs />
     </main>
   );
 }
---
+++ components/organism/landing/ContactUs/index.tsx
@@ -0,0 +1,26 @@
+import ContainerWrapper from "@/components/Layout/ContainerWrapper";
+import MaxWidthWrapper from "@/components/Layout/MaxWidthWrapper";
+import BannerCardContactUs from "@/components/molecule/Cards/BannerCardContactUs";
+import FormCard from "@/components/molecule/Cards/FormCard";
+import React from "react";
+
+const ContactUs = () => {
+  return (
+    <div>
+      <MaxWidthWrapper>
+        <ContainerWrapper
+          heading="Contact"
+          headingSpan="Us"
+          subheading="Contact us to book an appointment"
+        >
+          <div className="flex w-full flex-auto gap-[40px] justify-center items-center">
+            <BannerCardContactUs />
+            <FormCard title={"Schedule an"} titleSpan={"Appointment"} />
+          </div>
+        </ContainerWrapper>
+      </MaxWidthWrapper>
+    </div>
+  );
+};
+
+export default ContactUs;
