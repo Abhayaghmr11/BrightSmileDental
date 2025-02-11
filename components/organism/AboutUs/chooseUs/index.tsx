3d96ccf16b8c27bd19accd53703aadbe28e0c1e3
Abhayaghmr11 <abhaya2057@outlook.com>
2025-02-11 16:48:58 +0000

    Fixed width error for Choose Us Cards

--- components/organism/AboutUs/chooseUs/index.tsx
+++ components/organism/AboutUs/chooseUs/index.tsx
@@ -6,27 +6,34 @@ import React from 'react'
 import Chooseus from "@/public/assets/images/Chooseus.png"
 
 const ChooseUs = () => {
-  return (
-    <MaxWidthWrapper>
-        <ContainerWrapper heading='Why Choose' headingSpan='Us'>
-            <div className='w-full flex flex-col md:flex-row flex-wrap'>
+    return (
+        <MaxWidthWrapper>
+            <ContainerWrapper
+                heading='Why Choose'
+                headingSpan='Us'>
+                <div className='flex flex-col md:flex-row flex-wrap'>
 
-            {/* Conatiner */}
-            <div className='max-w-[395px] h-[360px] flex flex-col gap-[10px] rounded-[20px] p-[10px] border border-border_color'>
-                {/* image */}
-                <div>
-                    <Image src={Chooseus} alt="Choose us" className='object-cover rounded-[10px] w-[375px] h-[220px] overflow-hidden'/> 
+                    {/* Conatiner */}
+                    <div className='max-w-[395px] flex flex-col gap-[10px] cursor-pointer rounded-[20px] p-[10px] border border-border_color'>
+                        {/* image */}
+                        <div>
+                            <Image 
+                                src={Chooseus} 
+                                alt="Choose us" 
+                                width={375}
+                                height={220}
+                                className='object-cover rounded-[10px] w-[375px] h-[220px] overflow-hidden' />
+                        </div>
+                        {/* heading test */}
+                        <div></div>
+                        {/* numbering */}
+                        <div></div>
+                    </div>
+
                 </div>
-                {/* heading test */}
-                <div></div>
-                {/* numbering */}
-                <div></div>
-            </div>
-
-            </div>
-        </ContainerWrapper>
-    </MaxWidthWrapper>
-  )
+            </ContainerWrapper>
+        </MaxWidthWrapper>
+    )
 }
 
 export default ChooseUs
