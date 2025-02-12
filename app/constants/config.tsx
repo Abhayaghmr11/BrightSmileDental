30a5bab414ca2a70b07874c4c420b6b04efcae42
AnishSherchan <sherchananish11@gmail.com>
2025-02-12 22:23:18 +0545

    UUID added

--- app/constants/config.tsx
+++ app/constants/config.tsx
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from "uuid";
+
 export const companyEmail = "brightsmiledental@gmail.com";
 export const phoneNumber = "+977 1-4519594";
 export const companyMoto =
@@ -13,41 +15,49 @@ export const ServiceData = [
     category: "Extraction",
     section: [
       {
+        id: uuidv4(),
         service: "Primary Tooth Extraction",
         details: "",
         price: "Rs. 500",
       },
       {
+        id: uuidv4(),
         service: "Grade I Extraction",
         details: "",
         price: "Rs. 500",
       },
       {
+        id: uuidv4(),
         service: "Grade II Extraction",
         details: "",
         price: "Rs. 500",
       },
       {
+        id: uuidv4(),
         service: "Upper Wisdom Tooth (Simple)",
         details: "",
         price: "Rs. 500",
       },
       {
+        id: uuidv4(),
         service: "Upper Wisdom Tooth (Complex)",
         details: "",
         price: "Rs. 500",
       },
       {
+        id: uuidv4(),
         service: "Surgical Extraction (Including Lower Wisdom Tooth)",
         details: "",
         price: "Rs. 500",
       },
       {
+        id: uuidv4(),
         service: "Extraction for Orthodontic Treatment",
         details: "",
         price: "Rs. 500",
       },
       {
+        id: uuidv4(),
         service:
           "Cyst Removal Surgery and APICOECTOMY (Per Tooth by Specialist)",
         details: "",
@@ -59,51 +69,61 @@ export const ServiceData = [
     category: "Endodontics Root Canal Treatment",
     section: [
       {
+        id: uuidv4(),
         service: "Anterior Root Canal Treatment",
         details: "",
         price: "Rs. 500",
       },
       {
+        id: uuidv4(),
         service: "Posterior Root Canal Treatment",
         details: "",
         price: "Rs. 500",
       },
       {
+        id: uuidv4(),
         service: "Re-Root Canal Treatment",
         details: "",
         price: "Rs. 500",
       },
       {
+        id: uuidv4(),
         service: "Single Visit RCT - Anterior",
         details: "",
         price: "Rs. 500",
       },
       {
+        id: uuidv4(),
         service: "Single Visit RCT - Posterior",
         details: "",
         price: "Rs. 500",
       },
       {
+        id: uuidv4(),
         service: "Instrument Bi-Pass",
         details: "",
         price: "Rs. 500",
       },
       {
+        id: uuidv4(),
         service: "Instrument Removal",
         details: "",
         price: "Rs. 500",
       },
       {
+        id: uuidv4(),
         service: "Crown Removal",
         details: "",
         price: "Rs. 500",
       },
       {
+        id: uuidv4(),
         service: "Pulpectomy",
         details: "",
         price: "Rs. 500",
       },
       {
+        id: uuidv4(),
         service: "3RD Molar RCT",
         details: "",
         price: "Rs. 500",
@@ -114,41 +134,49 @@ export const ServiceData = [
     category: "Periodontics",
     section: [
       {
+        id: uuidv4(),
         service: "Gingival Abcess Drainage",
         details: "",
         price: "Rs. 500",
       },
       {
+        id: uuidv4(),
         service: "Subgingival Scaling I",
         details: "",
         price: "Rs. 500",
       },
       {
+        id: uuidv4(),
         service: "Subgingival Scaling II",
         details: "",
         price: "Rs. 500",
       },
       {
+        id: uuidv4(),
         service: "Subgingival Scaling III",
         details: "",
         price: "Rs. 500",
       },
       {
+        id: uuidv4(),
         service: "Flap Surgery Sextant (Grade I)",
         details: "",
         price: "Rs. 500",
       },
       {
+        id: uuidv4(),
         service: "Flap Surgery Sextant (Grade II)",
         details: "",
         price: "Rs. 500",
       },
       {
+        id: uuidv4(),
         service: "Flap Surgery Quadrant (Grade I)",
         details: "",
         price: "Rs. 500",
       },
       {
+        id: uuidv4(),
         service: "Flap Surgery Quadrant (Grade II)",
         details: "",
         price: "Rs. 500",
@@ -159,21 +187,25 @@ export const ServiceData = [
     category: "Implant",
     section: [
       {
+        id: uuidv4(),
         service: "Implant",
         details: "",
         price: "Rs. 500",
       },
       {
+        id: uuidv4(),
         service: "Overlay Retainer",
         details: "",
         price: "Rs. 500",
       },
       {
+        id: uuidv4(),
         service: "Implant Maintenance",
         details: "",
         price: "Rs. 500",
       },
       {
+        id: uuidv4(),
         service: "SINUS Lift (Including Bone Graft and Membrane)",
         details: "",
         price: "Rs. 500",
@@ -184,21 +216,25 @@ export const ServiceData = [
     category: "Orthodontics",
     section: [
       {
+        id: uuidv4(),
         service: "Orthodontic Treatment (Braces)",
         details: "",
         price: "Rs. 500",
       },
       {
+        id: uuidv4(),
         service: "Clear Aligner Upper and Lower",
         details: "",
         price: "Rs. 500",
       },
       {
+        id: uuidv4(),
         service: "Overlay Retainer - Per Inch",
         details: "",
         price: "Rs. 500",
       },
       {
+        id: uuidv4(),
         service: "Hawleys Appliance - Per Arch",
         details: "",
         price: "Rs. 500",
@@ -209,91 +245,109 @@ export const ServiceData = [
     category: "Prosthodontics",
     section: [
       {
+        id: uuidv4(),
         service: "PFM / Metal Crown",
         details: "",
         price: "Rs. 15,000",
       },
       {
+        id: uuidv4(),
         service: "PFM / Metal Crown (Milled)",
         details: "",
         price: "Rs. 20,000",
       },
       {
+        id: uuidv4(),
         service: "Full Zirconia Crown",
         details: "",
         price: "Rs. 25,000",
       },
       {
+        id: uuidv4(),
         service: "Zirconia Permium",
         details: "",
         price: "Rs. 35,000",
       },
       {
+        id: uuidv4(),
         service: "E - Max Crown",
         details: "",
         price: "Rs. 18,000",
       },
       {
+        id: uuidv4(),
         service: "MaryLand PFM",
         details: "",
         price: "Rs. 20,000",
       },
       {
+        id: uuidv4(),
         service: "MaryLand Zirconia",
         details: "",
         price: "Rs. 35,000",
       },
       {
+        id: uuidv4(),
         service: "RPD Per Arch",
         details: "",
         price: "Rs. 15,000",
       },
       {
+        id: uuidv4(),
         service: "Complete Denture",
         details: "",
         price: "Rs. 55,000",
       },
       {
+        id: uuidv4(),
         service: "Complete Denture - Single Arch",
         details: "",
         price: "Rs. 35,000",
       },
       {
+        id: uuidv4(),
         service: "Flexible Denture Per Arch",
         details: "",
         price: "Rs. 25,000",
       },
       {
+        id: uuidv4(),
         service: "Denture Repair (with Impression) Grade I",
         details: "",
         price: "Rs. 3,500",
       },
       {
+        id: uuidv4(),
         service: "Denture Repair (with Impression) Grade II",
         details: "",
         price: "Rs. 5,000",
       },
       {
+        id: uuidv4(),
         service: "Cast Partial Denture - Per Arch",
         details: "",
         price: "Rs. 28,000",
       },
       {
+        id: uuidv4(),
         service: "Temporary Crown - Per Unit",
         details: "",
         price: "Rs. 2,000",
       },
       {
+        id: uuidv4(),
         service: "Crown Re-Cementation - Per tooth",
         details: "",
         price: "Rs. 1,500",
       },
       {
+        id: uuidv4(),
         service: "Deprogrammer",
         details: "",
         price: "Rs. 8,000",
       },
       {
+        id: uuidv4(),
         service: "Deprogrammer Double Layer",
         details: "",
         price: "Rs. 11,000",
@@ -304,21 +358,25 @@ export const ServiceData = [
     category: "Diagnostic",
     section: [
       {
+        id: uuidv4(),
         service: "Registration",
         details: "",
         price: "Rs. 1,000",
       },
       {
+        id: uuidv4(),
         service: "Follow Up",
         details: "",
         price: "Rs. 1,000",
       },
       {
+        id: uuidv4(),
         service: "X-Ray",
         details: "",
         price: "Rs. 500",
       },
       {
+        id: uuidv4(),
         service: "Consultation",
         details: "",
         price: "Rs. 1,500",
@@ -329,26 +387,31 @@ export const ServiceData = [
     category: "Preventive",
     section: [
       {
+        id: uuidv4(),
         service: "Oral Prophylaxis - Simple",
         details: "",
         price: "Rs. 3,500",
       },
       {
+        id: uuidv4(),
         service: "Oral Prophylaxis - Advance",
         details: "",
         price: "Rs. 5,000",
       },
       {
+        id: uuidv4(),
         service: "Oral Prophylaxis - Complex",
         details: "",
         price: "Rs. 8000",
       },
       {
+        id: uuidv4(),
         service: "Fluoride Application (Full Mouth)",
         details: "",
         price: "Rs. 2,000",
       },
       {
+        id: uuidv4(),
         service: "Night / Bite Guard - Per Arch",
         details: "",
         price: "Rs. 5,500",
@@ -359,16 +422,19 @@ export const ServiceData = [
     category: "Restorative",
     section: [
       {
+        id: uuidv4(),
         service: "Composite Filling Grade I",
         details: "",
         price: "Rs. 2,500",
       },
       {
+        id: uuidv4(),
         service: "Composite Filling Grade II",
         details: "",
         price: "Rs. 3,000",
       },
       {
+        id: uuidv4(),
         service: "Composite Filling Grade III",
         details: "",
         price: "Rs. 3,500",
@@ -379,16 +445,19 @@ export const ServiceData = [
     category: "Bleach",
     section: [
       {
+        id: uuidv4(),
         service: "Office Bleach",
         details: "",
         price: "Rs. 20,000",
       },
       {
+        id: uuidv4(),
         service: "Home Bleach with Tray (Upper and Lower)",
         details: "",
         price: "Rs. 15,000",
       },
       {
+        id: uuidv4(),
         service: "Internal Bleach per Tooth (per Tooth, per Sitting)",
         details: "",
         price: "Rs. 5,500",
